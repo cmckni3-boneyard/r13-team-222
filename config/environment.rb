96ef45c34e11da63341234c5cfcf0ac51bab70bd
Chris McKnight <cmckni3@gmail.com>
2013-10-20 15:01:45 -0500

    Change files to precompile Ignore precompiled assets folder

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,18 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+RailsBootstrap::Application.configure do
+  config.assets.precompile.shift
+
+  # Explicitly register the extensions we are interested in compiling
+  config.assets.precompile.push(Proc.new do |path|
+    File.extname(path).in? [
+      '.html', '.erb', '.haml',                 # Templates
+      '.png',  '.gif', '.jpg', '.jpeg',         # Images
+      '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+    ]
+  end)
+end
+
 # Initialize the Rails application.
 RailsBootstrap::Application.initialize!
